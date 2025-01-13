181a31c225a0571e89e0616818fb2dddf1301131
Gustavo Sachetto <g.sachettocruz@gmail.com>
2025-01-13 11:17:41 -0300

    chore: corrigi build do projeto

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client"
 
-export const socket = io('https://jogo-da-velha-9npjyi4qw-gustavosachettos-projects.vercel.app:3001')
+export const socket = io('localhost:3001')
